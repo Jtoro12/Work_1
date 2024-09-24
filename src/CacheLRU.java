0bb6e9e815aa35c2831ff5965879c853dbc8a2a4
jtoro12 <javier.toro2301@alumnos.ubiobio.cl>
2024-09-23 22:13:35 -0300

    usando la clase LRU como un main pero para los test de este mismo algoritmo

--- src/CacheLRU.java
+++ src/CacheLRU.java
@@ -1,3 +1,19 @@
 public class CacheLRU {
+    public static void main(String[] args) {
+        HashingDiccionario tabla= new HashingDiccionario(3);
+        tabla.insertar(10);
+        tabla.insertar(20);
+        tabla.insertar(30);
+        tabla.imprimirTabla();
+        tabla.imprimirLista();
+        System.out.println("ingresar un elemento de mas");
+        tabla.insertar(40);
+        tabla.imprimirLista();
+        tabla.imprimirTabla();
+        System.out.println("insertando uno nuevo");
+        tabla.insertar(50);
+        tabla.imprimirLista();
+        tabla.imprimirTabla();
 
+    }
 }
--- src/HashingDiccionario.java
+++ src/HashingDiccionario.java
@@ -102,22 +102,6 @@ public class HashingDiccionario {
         }
     }
 
-    public static void main(String[] args) {
-        HashingDiccionario tabla= new HashingDiccionario(3);
-        tabla.insertar(10);
-        tabla.insertar(20);
-        tabla.insertar(30);
-        tabla.imprimirTabla();
-        tabla.imprimirLista();
-        System.out.println("ingresar un elemento de mas");
-        tabla.insertar(40);
-        tabla.imprimirLista();
-        tabla.imprimirTabla();
-        System.out.println("insertando uno nuevo");
-        tabla.insertar(50);
-        tabla.imprimirLista();
-        tabla.imprimirTabla();
-
-    }
+   
 
 }
