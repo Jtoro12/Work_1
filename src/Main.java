a3c6580a9b3cf0a271a9ec74193bb03f0e6705fb
jtoro12 <javier.toro2301@alumnos.ubiobio.cl>
2024-10-03 13:20:35 -0300

    new uptade

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("test 1");
-        ArraysList nuevoArray=new ArraysList();
-        HashingEncadenado hash=new HashingEncadenado();
-        LinkedList linked=new LinkedList();
-    }
-}
