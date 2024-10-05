a6ee4309953b7b208de6c72784b84bb2ea95003a
jtoro12 <javier.toro2301@alumnos.ubiobio.cl>
2024-10-05 14:08:06 -0300

    agregacion metodo fibronacci a LFU

--- src/cacheLFU_/Main.java
+++ src/cacheLFU_/Main.java
@@ -4,7 +4,11 @@ public class Main {
     public static void main(String[] args) {
         int cacheSize=3;
         CacheLFU lfu=new CacheLFU(cacheSize);
-        
+        FibronacciLFU fibronacciLFU=new FibronacciLFU();
+        int test=2;
+        int resultado=(fibronacciLFU.fibonacciLRU(test, lfu));
+        System.out.println("Fibronacci[LFU] resultado de:"+test+"="+resultado);
+       
     }
     
 }
--- src/cacheLRU_/Main.java
+++ src/cacheLRU_/Main.java
@@ -1,16 +1,17 @@
 package cacheLRU_;
+
 public class Main {
     public static void main(String[] args) {
         int cacheSize = 3;
         LRUCache lruCache = new LRUCache(cacheSize);
-        FibonacciLRU fibonacciLRU=new FibonacciLRU();
-        int test=2;
-        int result=(fibonacciLRU.fibonacciLRU(test,lruCache));
-        System.out.println("Fibronacci resultado de:"+test+"="+result);
-       System.out.println("estados");
-       lruCache.print();
-    
-        
-        
+        FibonacciLRU fibonacciLRU = new FibonacciLRU();
+        int test = 2;
+        int result = (fibonacciLRU.fibonacciLRU(test, lruCache));
+        System.out.println("Fibronacci[LRU] resultado de:" + test + "=" + result);
+        /*
+         * System.out.println("estados");
+         * lruCache.print();
+         */
+
     }
 }
